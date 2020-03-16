4c92b8cdcdc506024cffa88c151048c1e9d2dbfc
McPepe26 <andyzam301@gmail.com>
2020-03-15 23:35:58 -0600

    Agregamos interfaz de comunicación entre la vista y el cliente

---
+++ Pong-online/src/Cliente/Interfaces/Comunicacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente.Interfaces;
+
+/**
+ *
+ * @author ja-za
+ */
+public interface Comunicacion {
+    void iniciarJuego();
+    void esJugador(boolean esJugador1);
+    void setParametros(int params[]);
+}
