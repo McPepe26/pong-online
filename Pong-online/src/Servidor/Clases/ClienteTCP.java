6b4ea47d2ee5e7f72bb698c7ef3bd7ef925f1c89
McPepe26 <andyzam301@gmail.com>
2020-03-15 21:05:18 -0600

    Agregue el metodo run para la comunicación en partida

--- Pong-online/src/Servidor/Clases/ClienteTCP.java
+++ Pong-online/src/Servidor/Clases/ClienteTCP.java
@@ -10,6 +10,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,7 +31,40 @@ public class ClienteTCP extends Thread{
         salida =  new DataOutputStream(socket.getOutputStream());
         entrada = new DataInputStream(socket.getInputStream());
     }
+    
+    public void mandar(String msg) throws IOException{
+        salida.writeUTF(msg);
+    }
+    
+    public String recibir() throws IOException{
+        return entrada.readUTF();
+    }
 
+    @Override
+    public void run() {
+        while(true){
+            try {
+                int[] datosPos = new int[6];
+                String datos = recibir();
+                for (int i = 0; datos.length() > 0; i++) {
+                    int fin = datos.indexOf("]");
+                    datosPos[i] = Integer.parseInt(datos.substring(1, fin));
+                    datos = datos.substring(fin + 1);
+                }
+                comunicacion.difusion("[" + datosPos[0] + "][" + datosPos[1] + "]" + 
+                                      "[" + datosPos[2] + "][" + datosPos[3] + "]" + 
+                                      "[" + datosPos[4] + "][" + datosPos[5] + "]");
+                if (info.equals(""))
+                    info = "[" + datosPos[0] + "][" + datosPos[1] + "]" + 
+                           "[" + datosPos[2] + "][" + datosPos[3] + "]" + 
+                           "[" + datosPos[4] + "][" + datosPos[5] + "]";
+                mandar(info);
+            } catch (IOException ex) {
+                Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     public String getInfo() {
         return info;
     }
