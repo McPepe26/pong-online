9a65a38a334be19cae44cf28606fd6b5d4dab121
McPepe26 <andyzam301@gmail.com>
2020-03-15 23:01:28 -0600

    Creando ClienteTCP para el cliente

---
+++ Pong-online/src/Cliente/Clases/ClienteTCP.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente.Clases;
+
+import Config.Interfaces.Config;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ja-za
+ */
+public class ClienteTCP extends Thread implements Config{
+    private Socket socket;
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+
+    public ClienteTCP() {
+        try {
+            socket = new Socket(HOST, PUERTO);
+            entrada = new DataInputStream(socket.getInputStream());
+            salida = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
