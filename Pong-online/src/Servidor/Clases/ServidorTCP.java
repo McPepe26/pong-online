a6f9f138387aaba8c70c346c2e4b2786d5a808f1
McPepe26 <andyzam301@gmail.com>
2020-03-15 21:11:37 -0600

    Agregue constructor a la clase ServidorTCP

--- Pong-online/src/Servidor/Clases/ServidorTCP.java
+++ Pong-online/src/Servidor/Clases/ServidorTCP.java
@@ -6,11 +6,28 @@
 package Servidor.Clases;
 
 import Config.Interfaces.Config;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ja-za
  */
 public class ServidorTCP extends Thread implements Config{
+    private ServerSocket server;
+    private LinkedList<ClienteTCP> clientes;
+
+    public ServidorTCP() {
+        try {
+            server = new ServerSocket(PUERTO);
+            clientes = new LinkedList<>();
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
