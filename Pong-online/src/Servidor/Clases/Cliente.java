b92563d84fc80fa62d69443bc58a51978d2a4119
McPepe26 <andyzam301@gmail.com>
2020-03-15 20:29:27 -0600

    Creación clase Cliente para el servidor

---
+++ Pong-online/src/Servidor/Clases/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servidor.Clases;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author ja-za
+ */
+public class Cliente extends Thread{
+    private Socket socket;
+    private DataOutputStream salida;    
+}
