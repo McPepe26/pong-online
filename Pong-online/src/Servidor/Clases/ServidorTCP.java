1c57cc77b79ed17a0b398481b2d4af0baefe1bdb
McPepe26 <andyzam301@gmail.com>
2020-03-15 20:19:59 -0600

    Creación clase ServidorTCP

---
+++ Pong-online/src/Servidor/Clases/ServidorTCP.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servidor.Clases;
+
+import Config.Interfaces.Config;
+
+/**
+ *
+ * @author ja-za
+ */
+public class ServidorTCP extends Thread implements Config{
+    
+}
