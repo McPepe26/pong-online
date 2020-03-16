f9fab231f0466f32ce670e8eadc5169fee633b3e
McPepe26 <andyzam301@gmail.com>
2020-03-15 20:19:01 -0600

    Configuracion inicial, primer commit

---
+++ Pong-online/src/Config/Interfaces/Config.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Config.Interfaces;
+
+/**
+ *
+ * @author ja-za
+ */
+public interface Config {
+    public final int PUERTO = 2000; //Puerto para la conexión
+    public final String HOST = "localhost"; //Host para la conexión
+    public final static String SERVIDOR = "S";
+    public final static String CLIENTE = "C";
+}
